46f43c39f262547acbed60860698409df02cb30c
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2022-11-22 20:01:11 +0600

    implement profile model by mongoose

---
+++ Models/Profile.js
@@ -0,0 +1,55 @@
+const {model, Schema} = require('mongoose')
+
+const profileSchema = new Schema({
+    fullName: {
+        type: String,
+        maxlength: 30,
+        trim: true,
+    },
+    bio: {
+        type:String,
+        maxlength: 500,
+        trim:true,
+    },
+    profilePic: {
+        type: String,
+    },
+    contactInfo: {
+        phone: Number,
+        email: String,
+        address: String,
+    },
+    ads: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Ads'
+        }
+    ],
+    blogs: [
+        {
+            type: Schema.Types.ObjectId,
+            ref:'Blog'
+        }
+    ],
+    Orders:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Order'
+        }
+    ],
+    Bookmarks: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Ads'
+        }
+    ],
+    Package: {
+        type:Schema.Types.ObjectId,
+        ref: 'Package'
+    },
+
+},{timestamps:true})
+
+const Profile = model('Profile',profileSchema)
+
+module.exports = Profile
